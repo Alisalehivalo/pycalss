7458fcea856b770174d2620ec9cbebc6afb35f9a
Ali Akbar Salehi <55212861+Alisalehivalo@users.noreply.github.com>
2021-08-05 11:13:10 +0430

    Add files via upload

---
+++ firstpy.py
@@ -0,0 +1,73 @@
+print('salam,man ali hastam.')
+print(12 * 50)
+print(26 / 3)
+print(26 // 3)
+print(26 ** 3)
+print((3 ** 3) * (5 ** 2))
+first_result = (3 **3)
+sec_result = (4 ** 2)
+third_result=((6+3) * 5)
+print(first_result)
+print(first_result * sec_result * third_result)
+tool = 4
+arz= 5
+ertefa=12
+hajm =tool * arz * ertefa
+print(hajm)
+name="ali"
+last="salehi"
+age=39
+print(name , last , age)
+print(type(name) , type(age))
+ghad= 178
+vazn= 66
+nesbat=ghad / vazn
+print(nesbat , type(nesbat))
+ghad= 178
+vazn= 66
+nesbat=ghad // vazn
+print(nesbat , type(nesbat))
+ghad= 178
+vazn= 66
+nesbat=float(ghad / vazn)
+print(nesbat , type(nesbat))
+py = int(3.14)
+print(py)
+print(name+last)
+print(name,last)
+print(name * 4)
+jomle ="salam khoobi?"
+jomle2="salam \nkhoobi\n?"
+jomle3="salam \tkhoobi\t?"
+print(jomle)
+print(jomle2)
+print(jomle3)
+jomle4="Ali Akbar Salehi \n \
+Ms. IT Engineering \n \
+Intl Mehrabad Airport \n \
+Android Developer"
+print(jomle4)
+name1= "ali"
+intro= f'salam man {name1} hastam'
+print(intro)
+a="salam"
+b=39
+c="salame"
+print(f'{a}\t{b}\t{c}')
+mo=f''' salam khoobi? {name1}
+chetori???'''
+print(mo)
+#print(mo)  for comment
+no=input()
+print("salam" ,no)
+print("tool:")
+t=input()
+print("arz:")
+a=input()
+m=int(t) * int(a)
+print(m)
+print("enter your name:")
+nam=input()
+print("salam",nam)
+str=f"salam {nam}"
+print(str)
---
+++ s2.py
@@ -0,0 +1,51 @@
+#name = 'ali'
+#if name == 'ali' :
+#    print("salam ali")
+#    print("again hello")
+#print("salam...")
+
+#name2=input('esmet chiye:\n')
+#if name2 == 'ali':
+#    print("hello" ,name2)
+#else:
+#    print("hello guest")
+
+#name3=input('esmet chiye:\n')
+#if name3 != 'reza':
+#    print("hello guest")
+#else:
+#    print("hello reza")
+
+#int=int(input('please enter a number:\n'))
+#if int > 100:
+#    print("bigger than 100")
+#else:
+#    if int == 100:
+#        print("number is 100")
+#    else: print("lower than 100")
+
+# name4=input('name:')
+# if name4 == 'ali':
+#     print("hello ali")
+# elif name4 == 'hasan':
+#     print("hello hasan")
+# elif name4 == 'saeed':
+#     print("hello saeed")
+# else: print("hello guest")
+
+# n1 =int(input('pls enter num1:'))
+# n2 =int(input('pls enter num2:'))
+# n3=input('pls enter amalgar')
+# if n3 == '+':
+#     n4=n1 + n2
+#     print(n4)
+# elif n3 =='-':
+#     n4=n1 - n2
+#     print(n4)
+# elif n3 =='*':
+#     n4=n1 * n2
+#     print(n4)
+# elif n3 =='/':
+#     n4=n1 / n2
+#     print(n4)
+# else: print("invalid")
